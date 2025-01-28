90fe7bc3c688d917a28ff58766918f0c93335737
Jacob Sherrill <jacobmsherrill1996@gmail.com>
2025-01-28 11:04:43 -0700

    Added attribution for third-party packages and create data access methods

---
+++ RecipeOrganizerLibrary/Methods/DatabaseConnectionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace RecipeOrganizerLibrary.Methods
+{
+    internal static class DatabaseConnectionHelper
+    {
+
+        /* this method utilizes the System.Configuration.Configuration Manager third-party package; please see the Credits.txt file for 
+         * licensing and copyright information in compliance with 
+        third-party package license agreements. */
+        internal static string ConnectionStringGetter(string databaseName)
+        {
+            return ConfigurationManager.ConnectionStrings[databaseName].ConnectionString;
+        }
+
+    }
+}
